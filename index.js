cc2ba0cf5649ec0849a09f9f0da99513a78621b6
zxvqaySSS <mathias.hogqvist@gmail.com>
2025-11-09 20:21:31 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,4 @@
+document.getElementById("button").onclick = function(){
+    var h1 = document.getElementById("smrdis");
+    h1.textContent = h1.textContent ? "" : "i knew you wouldn't listen >:( do not scroll down further"
+}
